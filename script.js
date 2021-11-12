dadf7e44f5c7f5b1f05ec51e16b3d4594eddb86a
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2021-11-12 20:48:36 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("togglePassword").addEventListener("click", () => {
+    const password = document.getElementById("password");
+    const icon = document.getElementById("togglePassword");
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+    }
+
+})
+
+{/* <i class="fas fa-eye-slash"></i> */ }
+
+// const fname = document.getElementById("first-name");
+// const lname = document.getElementById("last-name");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+
+const inputform = document.querySelectorAll('.inputform');
+
+inputform.forEach((input) => {
+    input.addEventListener("invalid", (events) => {
+        console.log("invalid");
+        if (input.value == '') {
+            input.nextElementSibling.classList.add("show");
+            input.classList.add("inputbox");
+        }
+    })
+
+    input.addEventListener('blur', (event) => {
+        if(input.checkValidity()){
+            input.classList.remove("inputbox")
+        }
+    })
+
+    // input.addEventListener("focus", () => {
+    //     if (input.classList.contains("show")) {
+    //         input.nextElementSibling.classList.remove("show");
+    //         input.classList.remove("inputbox")
+    //     }
+    // })
+})
